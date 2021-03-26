8086b23f29d649fea7b7445a1f348a71764fa32f
tanzhuobo <tanzhuobo@gmail.com>
2021-03-26 18:23:56 +0800

    weak_callback

--- dwater/base/weak_callback.h
+++ dwater/base/weak_callback.h
@@ -10,7 +10,7 @@
 #include <memory>
 
 namespace dwater {
-
+template<typename CLASS, typename... ARGS>
 
 ///
 /// 弱回调：如果对象还活着，就尝试调用它的成员函数，否则就不必调用
@@ -18,36 +18,45 @@ namespace dwater {
 /// 利用weak_ptr，在回调的时候 尝试提升为shared_ptr，如果提升成功，说明回调的对象
 /// 还活着，那么就可以执行回调，否则就不必执行。
 ///
-template<typename CLASS, typename... ARGS>
-class WeakCallback {
-public:
-    WeakCallback(const std::weak_ptr<CLASS>& object,
-                 const std::function<void (CLASS*, ARGS...)>& function)
-        : object_(object), function_(function) { }
+class WeakCallback
+{
+ public:
 
-    void operator()(ARGS&&... args) const {
-        std::shared_ptr<CLASS> ptr(object_.lock());
-        if ( ptr ) {
-            function_(ptr.get(), std::forward<ARGS>(args)...);
-        }
+  WeakCallback(const std::weak_ptr<CLASS>& object,
+               const std::function<void (CLASS*, ARGS...)>& function)
+    : object_(object), function_(function)
+  {
+  }
+
+  // Default dtor, copy ctor and assignment are okay
+
+  void operator()(ARGS&&... args) const
+  {
+    std::shared_ptr<CLASS> ptr(object_.lock());
+    if (ptr)
+    {
+      function_(ptr.get(), std::forward<ARGS>(args)...);
     }
+  }
 
-private:
-    std::weak_ptr<CLASS> object_;
-    std::function<void (CLASS*, ARGS...)> function_;
+ private:
+
+  std::weak_ptr<CLASS> object_;
+  std::function<void (CLASS*, ARGS...)> function_;
 };
 
-
 template<typename CLASS, typename... ARGS>
-WeakCallback<CLASS, ARGS...> makeWeakCallback(const std::shared_ptr<CLASS>& object,
-                                              void (CLASS::*function)(ARGS...)) {
-    return WeakCallback<CLASS, ARGS...>(object, function);
+WeakCallback<CLASS, ARGS...> MakeWeakCallback(const std::shared_ptr<CLASS>& object,
+                                              void (CLASS::*function)(ARGS...))
+{
+  return WeakCallback<CLASS, ARGS...>(object, function);
 }
 
 template<typename CLASS, typename... ARGS>
-WeakCallback<CLASS, ARGS...> makeWeakCallback(const std::shared_ptr<CLASS>& object,
-                                              void (CLASS::*function)(ARGS...) const) {
-    return WeakCallback<CLASS, ARGS...>(object, function);
+WeakCallback<CLASS, ARGS...> MakeWeakCallback(const std::shared_ptr<CLASS>& object,
+                                              void (CLASS::*function)(ARGS...) const)
+{
+  return WeakCallback<CLASS, ARGS...>(object, function);
 }
 
 } // dwater
